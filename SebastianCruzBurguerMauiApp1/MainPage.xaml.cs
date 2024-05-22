9775348f3c1772677b1bfc2f7aaf59a9dcf3244b
Sebastian Cruz <raul.cruz@udla.edu.ec>
2024-05-22 12:05:12 -0500

    Todo lo hecho en clase

--- SebastianCruzBurguerMauiApp1/MainPage.xaml.cs
+++ SebastianCruzBurguerMauiApp1/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace SebastianCruzBurguerMauiApp1
+﻿using Newtonsoft.Json;
+using SebastianCruzBurguerMauiApp1.Models;
+namespace SebastianCruzBurguerMauiApp1
 {
     public partial class MainPage : ContentPage
     {
@@ -10,7 +12,15 @@
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7015/api/");
+            var response = client.GetAsync("burger").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var burgers = response.Content.ReadAsStringAsync().Result;
+                var burgersList = JsonConvert.DeserializeObject<List<Burger>>(burgers);
+                listView.ItemsSource = burgersList;
+            }
         }
     }
 
--- SebastianCruzBurguerMauiApp1/Models/Burger.cs
+++ SebastianCruzBurguerMauiApp1/Models/Burger.cs
@@ -4,16 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SebastianCruzBurguerMauiApp1.Properties.Models
+namespace SebastianCruzBurguerMauiApp1.Models
 {
-
-    public class Burguer
+    internal class Burger
     {
+
         public int burgerId { get; set; }
         public string? name { get; set; }
         public bool withCheese { get; set; }
         public decimal precio { get; set; }
         public object[]? promos { get; set; }
     }
-
 }
